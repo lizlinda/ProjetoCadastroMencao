ab6bed6fd46915741cdea84ef33a153daa82228f
lizlinda <liz.ishidaa@gmail.com>
2023-05-26 21:24:54 +0900

    aula 26/05

--- www/js/motor.js
+++ www/js/motor.js
@@ -1,6 +1,7 @@
 var nome 
 var selecao
 var selecaodisciplina
+var selecaomensao
 
 function cadastrar(){
     nome = document.getElementById('inputnome').value
@@ -8,4 +9,21 @@ function cadastrar(){
 
     selecao = document.getElementById('disciplina')
     selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    selecao = document.getElementById('mencao')
+    selecaomensao = selecao.options[selecao.selectedIndex].value
+
+    localStorage.setItem(selecaodisciplina, selecaomensao)
+}
+
+function listar(){
+    var  exibirmensao
+
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    exibirmensao = localStorage.getItem(selecaodisciplina)
+
+    document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+    document.getElementById('exibirmensao').textContent = exibirmensao
 }
